c8e1f6be5359f854523b01877eae4260ad9149aa
Norawee Laohateerapong <StarlinzSama@gmail.com>
2024-06-23 14:32:26 +0700

    add side scrollers and modify some ui

--- RandomFood/RandomFood/ContentView.swift
+++ RandomFood/RandomFood/ContentView.swift
@@ -9,30 +9,86 @@ import SwiftUI
 
 
 struct ContentView: View {
+    @State var index: Int = 0
+    @State var meals: [String] = ["cubeSteakandGravy", "tomYumKoong", "florentineButterChicken"]
+    @State var drinks: [String] = ["mintJulep", "kirRoyale", "vesper"]
+    @State var desserts: [String] = ["granita", "drumstickPie", "keyLimePieMousse"]
     var body: some View {
-        VStack {
-            Text("🍛")
-                .font(.largeTitle)
-                .fontWeight(.bold)
-            Text("Gin Rai Dee?")
-                .font(.title)
-                .fontWeight(.bold)
+        ZStack {
+            Image("granita")
+                .resizable()
+                .opacity(0.2)
+                .cornerRadius(20)
+                .edgesIgnoringSafeArea(.all)
             
-            Button(action: {}, label: {
-                ZStack {
-                    RoundedRectangle(cornerRadius: 25)
-                        .frame(width: 200, height: 50)
-                    Text("Start your meal")
-                        .font(.title2)
-                        .fontWeight(.heavy)
-                        .foregroundColor(Color.white)
-                }
+            LinearGradient(gradient: Gradient(colors: [Color.orange.opacity(0.8), Color.clear]), startPoint: .top, endPoint: .bottom)
+                .edgesIgnoringSafeArea(.all)
+            
+
+
+            VStack {
+
+                Text("🍛")
+                    .font(.largeTitle)
+                    .fontWeight(.bold)
+                Text("Gin Rai Dee?")
+//                    .font(.title)
+                    .fontWeight(.bold)
+                    .font(.system(size: 34, weight: .heavy, design: .serif))
+                        .italic()
                 
-            })
-            
-        }
-    }
-}
+                Button(action: {}, label: {
+                    ZStack {
+                        RoundedRectangle(cornerRadius: 25)
+                            .frame(width: 200, height: 50)
+                        Text("Start your meal")
+//                            .font(.title2)
+    
+                            .foregroundColor(Color.white)
+                            .font(.system(size: 22, weight: .heavy, design: .serif))
+                                .italic()
+
+                    }
+                    
+                })
+                
+            }
+            VStack {
+                Spacer()
+                ScrollView(.horizontal, showsIndicators: false) {
+                    HStack(spacing: 20) {
+                        ForEach(0..<3) { index in
+                            Image("\(desserts[index])")
+                                .resizable()
+                                .frame(width: 200, height: 200)
+                                .cornerRadius(20)
+                                .padding(10)
+                                .shadow(radius: 10)
+                            Image("\(meals[index])")
+                                .resizable()
+                                .frame(width: 200, height: 200)
+                                .cornerRadius(20)
+                                .padding(10)
+                                .shadow(radius: 10)
+                            Image("\(drinks[index])")
+                                .resizable()
+                                .frame(width: 200, height: 200)
+                                .cornerRadius(20)
+                                .padding(10)
+                                .shadow(radius: 10)
+                            }
+
+                          }
+ 
+                         }
+
+                        }
+                    }
+                }
+            }
+
+
+
 
 #Preview {
     ContentView()
