2a595a2733220d370406bfbe8994a2def01d042e
Theeratdolchat Chatchai <sitthach7777@gmail.com>
2024-06-23 10:55:19 +0700

    first commit

---
+++ RandomFood/RandomFood/RandomFoodApp.swift
@@ -0,0 +1,17 @@
+//
+//  RandomFoodApp.swift
+//  RandomFood
+//
+//  Created by Theeratdolchat Chatchai on 23/6/2567 BE.
+//
+
+import SwiftUI
+
+@main
+struct RandomFoodApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
